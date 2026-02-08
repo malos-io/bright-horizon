b8ceeb20a8726253736a87988a091eb5b289b077
Al os, Marc (NBCUniversal) <Marc.Alos@nbcuni.com>
2026-02-07 19:37:44 -0500

    update build

--- frontend-admin/docker-entrypoint.sh
+++ frontend-admin/docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+cp /etc/nginx/conf.d/default.conf.template /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
--- frontend-public/docker-entrypoint.sh
+++ frontend-public/docker-entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 set -e
-
-# Substitute environment variables in nginx config
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Start nginx
+cp /etc/nginx/conf.d/default.conf.template /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
