888a1f77f25b7ed3163ec67f8c19c4e05de62b38
Al os, Marc (NBCUniversal) <Marc.Alos@nbcuni.com>
2026-02-14 03:51:56 -0500

    add analytics

--- frontend-public/src/main.js
+++ frontend-public/src/main.js
@@ -1,9 +1,17 @@
 import { createApp } from 'vue'
+import VueGtag from 'vue-gtag-next'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+
+const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
+if (gaMeasurementId) {
+  app.use(VueGtag, {
+    property: { id: gaMeasurementId },
+  }, router)
+}
 
 app.mount('#app')
